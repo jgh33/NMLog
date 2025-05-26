c529e51e6f8a93c6b2b6f548478d57618e01ab40
jiaoguohui <jiaoguohui@bigo.sg>
2025-05-26 11:29:48 +0800

    Initial commit

---
+++ Sources/NMLog/NMLog.swift
@@ -0,0 +1,47 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public struct NMLog  {
+    static public func info(tag: String? = nil, msg: String, file: String = #file, line: UInt = #line, function: String = #function) {
+        var message = "[D][\(file), \(function): (\(line))]"
+        if let tag {
+            message += "[\(tag)][\(msg)]"
+        } else {
+            message += "[\(msg)]"
+        }
+        print(msg)
+        
+    }
+    
+    static public func debug(tag: String? = nil, msg: String, file: String = #file, line: UInt = #line, function: String = #function) {
+        var message = "[i][\(file), \(function): (\(line))]"
+        if let tag {
+            message += "[\(tag)][\(msg)]"
+        } else {
+            message += "[\(msg)]"
+        }
+        print(msg)
+    }
+    
+    static public func warning(tag: String? = nil, msg: String, file: String = #file, line: UInt = #line, function: String = #function) {
+        var message = "[⚠️w][\(file), \(function): (\(line))]"
+        if let tag {
+            message += "[\(tag)][\(msg)]"
+        } else {
+            message += "[\(msg)]"
+        }
+        print(msg)
+    }
+    
+    static public func error(tag: String? = nil, msg: String, file: String = #file, line: UInt = #line, function: String = #function) {
+        var message = "[❌e][\(file), \(function): (\(line))]"
+        if let tag {
+            message += "[\(tag)][\(msg)]"
+        } else {
+            message += "[\(msg)]"
+        }
+        print(msg)
+    }
+    
+}
